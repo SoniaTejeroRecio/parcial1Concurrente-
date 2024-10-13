4fa62547abbf508b5d4369ea498cb21b1e06916b
Sira <sgonzmad@myuax.com>
2024-10-13 18:55:09 +0200

    modificar clase main para el funcionamiento con las nuevas clases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         BufferCompartido buffer = new BufferCompartido(10);
-        Scheduler scheduler = new Scheduler(3); // Tenemos 3 productores.
 
         // Creación de productores para diferentes componentes
-        Productor productor1 = new Productor(buffer, "Componente A", scheduler, 0);
-        Productor productor2 = new Productor(buffer, "Componente B", scheduler, 1);
-        Productor productor3 = new Productor(buffer, "Componente C", scheduler, 2);
+        Productor productor1 = new Productor(buffer, "Clavo");
+        Productor productor2 = new Productor(buffer, "Bola");
 
         // Creación de consumidor (línea de ensamblaje)
         Consumidor ensamblador = new Consumidor(buffer);
@@ -16,7 +14,6 @@ public class Main {
         // Inicio de los hilos
         productor1.start();
         productor2.start();
-        productor3.start();
         ensamblador.start();
     }
 }
