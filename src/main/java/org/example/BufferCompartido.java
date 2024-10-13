87908ad5a6f29e569dbdac053c1c2a37c4411c88
Sira <sgonzmad@myuax.com>
2024-10-13 18:51:03 +0200

    modificar clase bufferCompartido para el funcionamiento con las nuevas clases

--- src/main/java/org/example/BufferCompartido.java
+++ src/main/java/org/example/BufferCompartido.java
@@ -1,31 +1,33 @@
 package org.example;
 
+import org.example.Componentes.Componente;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class BufferCompartido {
-    private Queue<String> buffer = new LinkedList<>();
+    private Queue<Componente> buffer = new LinkedList<>();
     private int capacidad;
 
     public BufferCompartido(int capacidad) {
         this.capacidad = capacidad;
     }
 
-    public synchronized void producir(String componente) throws InterruptedException {
+    public synchronized void producir(Componente componente) throws InterruptedException {
         while (buffer.size() == capacidad) {
             wait(); //Espera si el búfer está lleno
         }
         buffer.add(componente);
-        System.out.println("Producido: " + componente);
+        System.out.println("Producido: " + componente.getNombre());
         notifyAll(); //Notifica a los consumidores que pueden consumir
     }
 
-    public synchronized String consumir() throws InterruptedException {
+    public synchronized Componente consumir() throws InterruptedException {
         while (buffer.isEmpty()) {
             wait(); //Espera si no hay componentes
         }
-        String componente = buffer.poll();
-        System.out.println("Consumido: " + componente);
+        Componente componente = buffer.poll();
+        System.out.println("Consumido: " + componente.getNombre());
         notifyAll(); //Notifica a los productores que pueden producir
         return componente;
     }
